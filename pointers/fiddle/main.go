53a02e5cfa1c63d3d451c895614e0a0a368fd14b
Brent Martin <bmart35@gmail.com>
2016-09-08 23:47:24 -0500

    test will memory address change when reassigned

--- pointers/fiddle/main.go
+++ pointers/fiddle/main.go
@@ -7,5 +7,20 @@ import (
 var a int
 func main() {
 	a = 1
+	fmt.Println("* Memory address does not change when reassigned *")
+	fmt.Println("")
+
+	fmt.Println("     The setup:")
+	fmt.Print("original address: ")
 	fmt.Println(&a)
+	fmt.Print("original value: ")
+	fmt.Println(a)
+
+	a = 2
+	fmt.Println("     Now lets reassign value from 1 to 2 - value changes, address stays")
+	fmt.Print("post-reassigned address: ")
+	fmt.Println(&a)
+	fmt.Print("post-reassigned value: ")
+	fmt.Println(a)
+
 }
